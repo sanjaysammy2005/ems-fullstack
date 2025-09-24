ac77ff32545a3d774b4f55a8215852005a2e443a
sanjaysammy2005 <sanjayyy19official@gmail.com>
2025-09-24 20:52:48 +0530

    done

---
+++ frontend/src/components/config.js
@@ -0,0 +1,4 @@
+const config = {
+  url: "http://localhost:2030/springbootemployeeapi/"  // WAR name matches backend deploy
+};
+export default config;
--- frontend/src/config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-  url: "http://localhost:2030/employeeapi/"  // WAR name matches backend deploy
-};
-export default config;
