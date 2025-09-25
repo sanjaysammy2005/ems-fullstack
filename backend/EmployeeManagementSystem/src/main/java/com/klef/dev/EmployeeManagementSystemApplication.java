c34aad61bffc43a0401c3409b7c6583b9ea11082
sanjaysammy2005 <sanjayyy19official@gmail.com>
2025-09-25 09:19:30 +0530

    updated

--- backend/EmployeeManagementSystem/src/main/java/com/klef/dev/EmployeeManagementSystemApplication.java
+++ backend/EmployeeManagementSystem/src/main/java/com/klef/dev/EmployeeManagementSystemApplication.java
@@ -2,15 +2,11 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class EmployeeManagementSystemApplication extends SpringBootServletInitializer {
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(EmployeeManagementSystemApplication.class);
-    }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
